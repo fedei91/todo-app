f96ceefe1ca59f5dd9b2073e27f98eceab203b5f
Federico <fedej.izaguirre@gmail.com>
2022-03-02 15:52:58 +0100

    add database and -schema

---
+++ src/main/resources/todoapp.sql
@@ -0,0 +1,23 @@
+set names utf8mb4;
+set charset utf8mb4;
+drop database if exists todoapp;
+create database todoapp charset utf8mb4;
+use todoapp;
+
+create table users (
+  id int unsigned not null auto_increment primary key,
+  email varchar(100) not null,
+`password` varchar(255) NOT NULL,
+enabled bit NOT NULL default 1);
+ 
+create table todoitems (
+	id int unsigned not null auto_increment primary key,
+    itemDescription varchar(100) not null,
+    itemDone boolean not null,
+    userId int unsigned not null,
+    constraint itemsUsers foreign key(userId) references users(id)
+);
+
+create user if not exists fedei91 identified by 'fedei91';
+grant select,insert on users to fedei91;
+grant select,insert,update on todoitems to fedei91;
