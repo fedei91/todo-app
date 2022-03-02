7e3fa872fe65646f08e796434d60b9fb7d4d62ce
Federico <fedej.izaguirre@gmail.com>
2022-03-02 14:42:01 +0100

    initial commit

---
+++ src/test/java/be/fedei91/todoapp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package be.fedei91.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
