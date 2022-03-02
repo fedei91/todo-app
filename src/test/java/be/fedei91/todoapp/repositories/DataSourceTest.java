c74b7acce939176b7bdea20e3969741971c77899
Federico <fedej.izaguirre@gmail.com>
2022-03-02 15:53:44 +0100

    test database connection

---
+++ src/test/java/be/fedei91/todoapp/repositories/DataSourceTest.java
@@ -0,0 +1,25 @@
+package be.fedei91.todoapp.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class DataSourceTest {
+    private final DataSource dataSource;
+
+    DataSourceTest(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Test
+    void getConnection() throws SQLException {
+        try (var connection = dataSource.getConnection()) {
+            assertThat(connection.getCatalog()).isEqualTo("todoapp");
+        }
+    }
+}
