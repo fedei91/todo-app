bd07f9fa54269ced5ca06879a3fff3e1a6f73f0b
Federico <fedej.izaguirre@gmail.com>
2022-03-10 15:43:24 +0100

    create User repository, service and implementation

---
+++ src/main/java/be/fedei91/todoapp/services/DefaultUserService.java
@@ -0,0 +1,30 @@
+package be.fedei91.todoapp.services;
+
+import be.fedei91.todoapp.domain.User;
+import be.fedei91.todoapp.exceptions.UserAlreadyExistException;
+import be.fedei91.todoapp.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+class DefaultUserService implements UserService{
+    private final UserRepository userRepository;
+
+    DefaultUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void create(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
---
+++ src/main/java/be/fedei91/todoapp/services/UserService.java
@@ -0,0 +1,11 @@
+package be.fedei91.todoapp.services;
+
+import be.fedei91.todoapp.domain.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    void create(User user);
+    List<User> findAll();
+}
