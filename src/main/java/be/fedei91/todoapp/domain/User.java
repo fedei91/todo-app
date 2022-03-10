a0bcf509fa7d74266d62f974c872c858705a722d
Federico <fedej.izaguirre@gmail.com>
2022-03-10 10:50:43 +0100

    create User class

---
+++ src/main/java/be/fedei91/todoapp/domain/User.java
@@ -0,0 +1,45 @@
+package be.fedei91.todoapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    protected User() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
